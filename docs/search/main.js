abb08feb10deb7011a1efafe6500a0f17cbbbf97
dann <gh@mrmuster.com>
2025-04-26 13:21:10 -0400

    A searchable timeline

---
+++ docs/search/main.js
@@ -0,0 +1,57 @@
+const el = document.getElementById.bind(document)
+const events = []
+const renderAll = throttle(renderEvents)
+
+function throttle(fun, wait = 100) { // throttle but call it again at the end
+  let throttled
+  return function(...args) {
+    if(throttled) return false
+    throttled = true
+    setTimeout(() => { throttled = false; fun(...args) }, wait)
+    fun(...args)
+  }
+}
+
+function renderEvents() {
+  let search = el('search').value.toLowerCase()
+  let str = ''
+  events.filter(e => (e.title+e.details).toLowerCase().includes(search)).forEach(event => {
+    str += eventTemplate(event)
+  })
+  el('timeline').innerHTML = str
+}
+
+function eventTemplate(event) {
+  return `
+    <details>
+      <summary><span class="date">${new Date(event.startDate).toLocaleString('en-CA', {year: 'numeric', month: 'short', day: '2-digit'})}</span> :: ${event.title}</summary>
+      <p class="tags">Tags: ${event.tags.join(', ')}</p>
+      <p class="hosts">Hosts: ${event.hosts.join(', ')}</p>
+      <p class="image">${event.image ? `<img src="${event.image}">` : ''}</p>
+      <div class="event_details">${event.details.replace(/\n/g, '<br>')}</div>
+    </details>`
+}
+
+function fetchAll(data) {
+  data.forEach(eventStub => {
+    const srcPath = `events/${eventStub.file}`
+    fetchJSON(srcPath, event => {
+      event.time = (new Date(event.startDate)).getTime()
+      events.push(event)
+      events.sort((a, b) => b.time - a.time) // wasteful
+      renderAll()
+    })
+  })
+}
+
+function fetchJSON(srcPath, callback) {
+  const prefix = 'https://raw.githubusercontent.com/torontojs/historical-archive/main/src/data/'
+  fetch(prefix + srcPath)
+  .then(resp => resp.json())
+  .then(data => callback(data))
+  .catch(err => console.error('Fetch went awry', err, srcPath, callback))
+}
+
+// init
+fetchJSON('directory.json', fetchAll)
+el('search').addEventListener('input', renderAll)
