c19dfb0acd21ea5ea5abe071f888239476cfa9b3
madcampos <me@madcampos.dev>
2025-06-05 23:02:57 -0400

    feat: add images and format files

--- extract-images.ps1
+++ extract-images.ps1
@@ -1,22 +1,33 @@
-Get-ChildItem '.\src\data\events' -File |
+$EventsFolder = '.\src\data\events'
+$ImagesFolder = "$EventsFolder\images"
+
+if (-not (Test-Path $ImagesFolder)) {
+	New-Item -Path $ImagesFolder -ItemType Directory | Out-Null
+}
+
+Get-ChildItem $EventsFolder -File |
 ForEach-Object {
 	$FilePath = $_
 	$FileContents = Get-Content $FilePath | ConvertFrom-Json
 
-	# TODO: get all matches
-	$FileContents.details -match 'http\S+?\.(?:jpg|jpeg|png|gif|webp)' | Out-Null
+	$ImageMatches = [regex]::Matches($FileContents.details, '\(http\S+?\.(?:jpg|jpeg|png|gif|webp)\)')
 
-	if ($Matches[0]) {
-		$ImageUrl = $Matches[0]
+	$ImageMatches | ForEach-Object {
+		$ImageUrl = [regex]::Replace($_.Value, '\(|\)', '')
 		$ImageFile = Split-Path $ImageUrl -Leaf
+		$ImagePath = "$ImagesFolder\$ImageFile"
 
 		try {
-			Invoke-WebRequest -Uri $ImageUrl -UseBasicParsing -OutFile ".\src\data\events\$ImageFile"
+			if (-not (Test-Path $ImagePath)) {
+				Invoke-WebRequest -Uri $ImageUrl -UseBasicParsing -OutFile $ImagePath
+			}
 
-			# TODO: replace urls with paths
-			$FileContents | ConvertTo-Json | Out-File $FilePath
+			$UnixFilePath = $ImagePath -replace @('\\', '/')
+			$FileContents.details = $FileContents.details -replace @([regex]::Escape($ImageUrl), $UnixFilePath)
 		} catch {
 			Write-Error "Error downloading: $ImageUrl"
 		}
 	}
+
+	$FileContents | ConvertTo-Json | Out-File $FilePath
 }
